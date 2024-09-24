28adfb34688d533bc912fac07cd2c7107799a9be
2104mari <mariaangelasilvasantos993@gmail.com>
2024-09-24 11:13:29 -0300

    Add files via upload

---
+++ maria. c.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int numero1 = 6;
+ int numero2 = 20;
+ int soma;
+ soma = numero1 + numero2;
+ printf("%d", soma);
+ char letra ='7';
+ char nome [5]={"maria"};
+ printf("%c",letra);
+ printf("%s",nome);
+printf("%c",letra);
+printf("%s",nome);
+}
