3c9965a1c8472b698e5741e8399c864d263ce203
2104mari <mariaangelasilvasantos993@gmail.com>
2024-10-21 12:53:08 -0300

    Add files via upload

---
+++ an.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int numero;
+    printf("o que quer fazer");
+    scanf("%d",&numero);
+    if(numero==1){
+        printf(" qual o saldo");
+    }else if (numero==2){
+        printf("qual o extrato");
+    }else if (numero==3){
+        printf("qual o saque");
+    }else if (numero==4){
+    
+    }else{
+       printf("operaçõa invalida");
+
+    }
+
+    
+
+}
