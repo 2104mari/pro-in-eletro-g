593e9a6c152bac84f3cdd9b76b0225f51545b7d9
2104mari <mariaangelasilvasantos993@gmail.com>
2024-10-23 14:45:56 -0300

    Add files via upload

---
+++ Idade.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+ int numero1 = 6;
+ int numero2 = 20;
+ int soma;
+ soma = numero1 + numero2;
+ printf("%d", soma);
+ char letra ='7';
+ char nome [5]={"maria"};
+ printf("%c",letra);
+ printf("%s",nome);
+printf("%c",letra);
+printf("%s",nome);
+int idade;
+printf("Informe sua idade:");
+ scanf("%d,&idade");
+ printf("%d",idade);
+}
