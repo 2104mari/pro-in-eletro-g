0464ff01c22907bbb5e76c40d98f6f6e85fc5546
2104mari <mariaangelasilvasantos993@gmail.com>
2024-10-23 15:28:43 -0300

    Add files via upload

---
+++ triagulo.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+    int lado1;
+    int lado2;
+    int lado3;
+    printf("informe o primeiro numero");
+    scanf("%d", &lado1);
+    printf("informe o segundo numero");
+    scanf("%d", &lado2);
+    printf("informe o terceiro numero");
+    scanf("%d", &lado3);
+
+    if((lado1+lado2)>lado3){
+       printf("e um triagulo");
+       if (lado1==lado2 && lado1==lado3){
+        printf("e um equilatero");
+
+       }else if (lado1!=lado2 && lado1!=lado3 && lado2!=lado3){
+        printf("e um escaleno");
+       }else{
+        printf("nao e um triagulo");
+       }
+       
+    }
+}
