396dfd38d11ecb3d76b04b9983a6447db5b7e094
2104mari <mariaangelasilvasantos993@gmail.com>
2024-11-25 11:12:21 -0300

    Add files via upload

---
+++ capital juros meses.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    float c;
+    float t;
+    int m;
+    int i;
+    float mont=0;
+    printf("qual o eu capital");
+    scanf("%f", &c);
+    printf("qual a taxa mensal");
+    scanf("%f",&t);
+    printf("qual o mes");
+    scanf("%d", &m);
+    for(i=1; i<=m; i++){
+mont=c+t;
+printf("%dmont: %f\n",i,mont);
+    }
+}
